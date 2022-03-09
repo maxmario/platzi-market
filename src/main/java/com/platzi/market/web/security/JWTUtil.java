92b578611a5784f0bdd3dd7cad8fe317b3973e42
Mario Guerrero <mario.guerrero.contreras@gmail.com>
2022-03-09 01:34:39 -0600

    spring security

---
+++ src/main/java/com/platzi/market/web/security/JWTUtil.java
@@ -0,0 +1,23 @@
+package com.platzi.market.web.security;
+
+import java.util.Date;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JWTUtil {
+
+    private static final String KEY = "pl4tz1";
+    
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+            .setSubject(userDetails.getUsername())
+            .setIssuedAt(new Date())
+            .setIssuedAt(new Date(System.currentTimeMillis() + 1000 + 60 + 60 + 10))
+            .signWith(SignatureAlgorithm.HS256, KEY).compact();
+    }
+}
